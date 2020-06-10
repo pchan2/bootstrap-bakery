aa2499bf97369fa649a5a3f1f28cbc4bff8a1f61
wonder woman <ww@dccomics.com>
2020-06-10 16:57:12 +0100

    Completed Julio Codes Tutorial
    Cake website using Bootstrap.

--- modernbootsite/scripts.js
+++ modernbootsite/scripts.js
@@ -1,1 +1,12 @@
+$(function () {
+    $(".hamburger-menu").on("click", function () {
+        $(".toggle").toggleClass("open");
+        $(".nav-list").toggleClass("open");
+    });
 
+    // animation to fade in content
+    AOS.init({
+        easing: "ease",
+        duration: 1000
+    });
+});
